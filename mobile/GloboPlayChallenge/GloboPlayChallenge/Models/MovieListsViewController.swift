aa48decca23510a4f288e9664e61ae3c5ecba17c
ana <ana@MacBook-Air-de-ana.local>
2025-10-01 01:25:58 -0300

    implementa SplashViewController como tela inicial via SceneDelegate e adiciona imagem no Assets

---
+++ mobile/GloboPlayChallenge/GloboPlayChallenge/Models/MovieListsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListsViewController.swift
+//  GloboPlayChallenge
+//
+//  Created by ana on 01/10/25.
+//
+
+import UIKit
+
+class MovieListsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        title = "Filmes"
+    }
+}
