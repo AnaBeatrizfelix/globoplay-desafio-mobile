4b5531a73e93317759b4ba64f0b3afd4bbf96041
ana <ana@MacBook-Air-de-ana.local>
2025-10-02 02:55:05 -0300

    adiciona implementacao inicial da HomeViewController com tabela, secoes e navbar personalizada

--- mobile/GloboPlayChallenge/GloboPlayChallenge/Controllers/HomeViewController.swift
+++ mobile/GloboPlayChallenge/GloboPlayChallenge/Controllers/HomeViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    
+    
+    let sectionTitles: [String] = ["Novelas", "Séries", "Cinema"]
+    
+    
     private let homeFeedtable: UITableView = {
-        let table: UITableView = UITableView(frame: .zero, style: .grouped)
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
         return table
     }()
     
@@ -22,27 +27,49 @@ class HomeViewController: UIViewController {
         
         homeFeedtable.delegate = self
         homeFeedtable.dataSource = self
-        
-
-      
+            
+        configureNavBar()
     }
+    
+    private func configureNavBar() {
+        
+        guard let image = UIImage(named: "logoGP") else { return }
+ 
+        let resizedImage = resizeImage(image: image, targetSize: CGSize(width: 120, height: 110))
+        
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: resizedImage, style: .done, target: self, action: nil)
+        navigationController?.navigationBar.tintColor = .white
+       }
+    
+    private func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+        return renderer.image { _ in
+            image.draw(in: CGRect(origin: .zero, size: targetSize))
+        }
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         homeFeedtable.frame = view.bounds
     }
-
+    
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitles.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .red
-        cell.textLabel?.text = "Hello word"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
         return cell
     }
     
@@ -51,5 +78,25 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 40    }
+        return 40
+    }
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection indexPath:Int) {
+        guard let header = view as? UITableViewHeaderFooterView else { return }
+        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+        header.textLabel?.textColor = .white
+        header.textLabel?.text = header.textLabel?.text?.lowercased()
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+    }
 }
