d5fdcbd3e35f3aa875bee8714947d9fa0e2912e1
ana <ana@MacBook-Air-de-ana.local>
2025-10-02 03:49:57 -0300

    adiciona filtros Cinema, Series e Novelas no MovieService com enum FilterType e suporte ao genero para novelas

--- mobile/GloboPlayChallenge/GloboPlayChallenge/Services/MovieService.swift
+++ mobile/GloboPlayChallenge/GloboPlayChallenge/Services/MovieService.swift
@@ -5,14 +5,31 @@ enum MovieServiceError: Error {
     case decodingFailed
     case invalidResponse
 }
+enum FilterType {
+    case cinema
+    case series
+    case novelas
+    
+    var path: String {
+        switch self {
+        case .cinema:
+            return "/movie/popular"
+        case .series:
+            return "/tv/popular"
+        case .novelas:
+            return "/tv/popular"
+        }
+    }
+}
 
 struct MovieService {
     
     private let baseURL = "https://api.themoviedb.org/3"
     private let apiKey = "6ab31bc2eb05690d1679fef51b5b25b5"
     
-    func getMovies() async throws -> [Movie] {
-    let urlString = "\(baseURL)/movie/popular?api_key=\(apiKey)&language=pt-BR&page=1"
+    func fetch(by filter: FilterType) async throws -> [Movie] {
+        let urlString = "\(baseURL)\(filter.path)?api_key=\(apiKey)&language=pt-BR&page=1"
+        
         guard let url = URL(string: urlString) else {
             throw MovieServiceError.invalidURL
         }
@@ -24,9 +41,11 @@ struct MovieService {
         }
         do {
             let movieResponse = try JSONDecoder().decode(MovieResponse.self, from: data)
+            if filter == .novelas {
+                return movieResponse.results.filter {  $0.genreIds.contains(10766) }
+            }
             return movieResponse.results
-        } catch (let error) {
-            print(error)
+        } catch {
             throw MovieServiceError.decodingFailed
             
             
